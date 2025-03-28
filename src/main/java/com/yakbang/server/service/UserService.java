c84e7b167a0b94edccc829d103256a37b934a85e
daeun <tmzl235@naver.com>
2025-03-29 01:05:15 +0900

    feat: 개발 환경 세팅 및 회원가입 API 작성

---
+++ src/main/java/com/yakbang/server/service/UserService.java
@@ -0,0 +1,31 @@
+package com.yakbang.server.service;
+
+import com.yakbang.server.context.StatusCode;
+import com.yakbang.server.dto.request.SignUpRequest;
+import com.yakbang.server.dto.response.DefaultResponse;
+import com.yakbang.server.entity.User;
+import com.yakbang.server.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+
+    public ResponseEntity<DefaultResponse> signUp(SignUpRequest request) {
+        // 유저 생성 및 저장
+        User user = User.create(request.userId(), request.password(), request.name());
+        userRepository.save(user);
+
+        return new ResponseEntity<>(DefaultResponse.from(StatusCode.OK, "회원가입 성공"),
+                HttpStatus.OK);
+    }
+
+    public ResponseEntity<DefaultResponse> nickname(String userId) {
+        return new ResponseEntity<>(DefaultResponse.from(StatusCode.OK, userRepository.findByUserId(userId).getName()),
+                HttpStatus.OK);
+    }
+}
